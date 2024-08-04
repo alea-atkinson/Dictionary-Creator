29eca1667f609843ac565de01dca48c72d5b20ea
maite <chaparteguimaite@ufl.edu>
2024-08-04 00:44:38 -0500

    commit

--- dictionary.cpp
+++ dictionary.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <algorithm>
 
+Dictionary::WordEntry::WordEntry() : searchCount(0) {}
+
 Dictionary::WordEntry::WordEntry(const std::string& w, const std::string& def, const std::vector<std::string>& syns)
         : word(w), definition(def), synonyms(syns), searchCount(0) {}
 
@@ -12,18 +14,82 @@ void Dictionary::addWord(const std::string& word, const std::string& definition,
 }
 
 void Dictionary::heapify(int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && words[left].word > words[largest].word)
+        largest = left;
+
+    if (right < n && words[right].word > words[largest].word)
+        largest = right;
+
+    if (largest != i) {
+        std::swap(words[i], words[largest]);
+        heapify(n, largest);
+    }
 }
 
 void Dictionary::heapSortAlphabetically() {
+    int n = words.size();
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(n, i);
+
+    for (int i = n - 1; i >= 0; i--) {
+        std::swap(words[0], words[i]);
+        heapify(i, 0);
+    }
 }
 
 void Dictionary::merge(int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    std::vector<WordEntry> L(n1);
+    std::vector<WordEntry> R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = words[l + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = words[m + 1 + i];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i].searchCount <= R[j].searchCount) {
+            words[k] = L[i];
+            i++;
+        } else {
+            words[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        words[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        words[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
 void Dictionary::mergeSort(int l, int r) {
+    if (l >= r)
+        return;
+
+    int m = l + (r - l) / 2;
+    mergeSort(l, m);
+    mergeSort(m + 1, r);
+    merge(l, m, r);
 }
 
 void Dictionary::mergeSortBySearchCount() {
+    mergeSort(0, words.size() - 1);
 }
 
 int Dictionary::binarySearch(const std::string& word) const {
@@ -52,20 +118,20 @@ bool Dictionary::searchWord(const std::string& word, std::string& definition, st
 }
 
 void Dictionary::sortAlphabetically() {
+    heapSortAlphabetically();
 }
 
 void Dictionary::sortBySearchCount() {
+    mergeSortBySearchCount();
 }
 
 void Dictionary::displayAllWords() const {
     for (const auto& entry : words) {
         std::cout << "Word: " << entry.word << "; Definition: " << entry.definition << "; Synonyms: ";
 
-        // Print synonyms
         if (!entry.synonyms.empty()) {
             for (size_t i = 0; i < entry.synonyms.size(); ++i) {
                 std::cout << entry.synonyms[i];
-                // Print a comma if it's not the last synonym
                 if (i < entry.synonyms.size() - 1) {
                     std::cout << ", ";
                 }
--- dictionary.h
+++ dictionary.h
@@ -1,36 +1,40 @@
-#ifndef DICTIONARY_CREATOR_DICTIONARY_H
-#define DICTIONARY_CREATOR_DICTIONARY_H
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
 
-#include <vector>
 #include <string>
+#include <vector>
 
 class Dictionary {
-private:
+public:
     struct WordEntry {
         std::string word;
         std::string definition;
         std::vector<std::string> synonyms;
         int searchCount;
 
+        WordEntry();
         WordEntry(const std::string& w, const std::string& def, const std::vector<std::string>& syns);
     };
 
-    std::vector<WordEntry> words;
+    Dictionary();
+
+    void addWord(const std::string& word, const std::string& definition, const std::vector<std::string>& synonyms);
 
     void heapSortAlphabetically();
     void mergeSortBySearchCount();
+
+    bool searchWord(const std::string& word, std::string& definition, std::vector<std::string>& synonyms) const;
+    void sortAlphabetically();
+    void sortBySearchCount();
+    void displayAllWords() const;
+
+private:
+    std::vector<WordEntry> words;
+
     void heapify(int n, int i);
     void merge(int l, int m, int r);
     void mergeSort(int l, int r);
     int binarySearch(const std::string& word) const;
-
-public:
-    Dictionary();
-    void addWord(const std::string& word, const std::string& definition, const std::vector<std::string>& synonyms);
-    void sortAlphabetically();
-    void sortBySearchCount();
-    bool searchWord(const std::string& word, std::string& definition, std::vector<std::string>& synonyms) const;
-    void displayAllWords() const;
 };
 
-#endif
+#endif // DICTIONARY_H
